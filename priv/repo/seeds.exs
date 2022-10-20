6ea4ce873fd33ad34123966232488b9848312521
kelium jojoa <keliumju@gmail.com>
2022-10-19 21:01:13 -0500

    dockerizing project for dev and test mode
    change the readme.md, quit blank spaces, rewrite the seeds.exs file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,9 +9,15 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+  alias Mintacoin.{Repo, Blockchain}
 
-alias Mintacoin.{Repo, Blockchain}
+  network = Application.get_env(:mintacoin, :blockchains_network, :testnet)
 
-network = Application.get_env(:mintacoin, :blockchains_network, :testnet)
+  run_seeds = fn 
+    false -> Repo.insert!(%Blockchain{name: Blockchain.default(), network: network})
+    true -> :ok 
+  end
 
-Repo.insert!(%Blockchain{name: Blockchain.default(), network: network})
+  Blockchain
+    |> Repo.exists?()
+    |> run_seeds.()
